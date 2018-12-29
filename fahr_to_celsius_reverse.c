46ffed6be2c15c8a249218696eb2426f966d6ae7
peteryu234 <peteryu234@126.com>
2018-12-29 20:13:24 +0800

    page 9

--- fahr_to_celsius_reverse.c
+++ fahr_to_celsius_reverse.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
 int main(void)
 {
     float fahr, celsius;
 
     printf("fahr to celsius\n");
 
-    for (int i = 300; i >= 0; i -= 20)
+    for (int i = UPPER; i >= LOWER; i -= STEP)
     {
         fahr = i;
         celsius = (fahr - 32.0) * 5.0 / 9.0;
