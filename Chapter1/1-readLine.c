3579f0276254373c36adfabf33f3ce2a5002881e
Peter Yu <peteryu234@126.com>
2020-09-02 15:59:28 +0800

    update readLine.c

---
+++ Chapter1/1-readLine.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#define MAXLINE 1000	/* 允许的输入行的最大长度 */
+
+int getLine(char line[], int maxline);
+void copy(char to[], char from[]);
+
+/* 打印最长的输入行 */
+int main(void)
+{
+    int len;				/* 当前行长度 */
+    int max = 0;			/* 目前为止发现的最长行的长度 */
+	char line[MAXLINE];		/* 当前的输入行 */
+	char longest[MAXLINE];	/* 用于保存最长的行 */
+
+    while ((len = getLine(line, MAXLINE)) > 0)
+        if (len > max) {
+            max = len;
+            copy(longest, line);
+        }
+    if (max > 0)	/* 存在这样的行 */
+        printf("%s", longest);
+	return 0;
+}
+
+/* getLine函数：将一行读入到s中并返回其长度 */
+int getLine(char s[], int lim)
+{
+    int c, i;
+
+    for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
+        s[i] = c;
+    if (c == '\n') {
+        s[i] = c;
+        ++i;
+    }
+    s[i] = '\0';
+    return i;
+}
+
+/* copy函数：将from复制到to;这里假定to足够大 */
+void copy(char to[], char from[])
+{
+    int i = 0;
+
+    while ((to[i] = from[i]) != '\0')
+        ++i;
+}
